69520b7b084559535978c51d9eed4322305cb1d9
Meryammmm <smairem1810@gmail.com>
2024-04-27 19:18:42 +0100

    first push

---
+++ src/product.js
@@ -0,0 +1,7 @@
+const product = {
+    name:'gaucho',
+    price:'300',
+    desription:'bzeyed choclata',
+    Image:'https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.saida-group.com%2Fproduits%2F13%2Fgaucho.html&psig=AOvVaw3LGKQLRaajeUge2741IJhI&ust=1713181649186000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOD7ypDRwYUDFQAAAAAdAAAAABAE',
+}
+export default product;
